f3e7e0597488fdc3bd349c47805a509358338c45
Cole Stuedeman <cstuedeman@gmail.com>
2026-03-28 00:58:21 -0500

    test commit to new branch

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 import AuthorImage from "../../images/author_thumbnail.jpg";
 import nftImage from "../../images/nftImage.jpg";
-
+//starting
 const NewItems = () => {
   return (
     <section id="section-items" className="no-bottom">
