87ecb4b13f0e095cc969e03800dd5333cb649ffa
Cole Stuedeman <cstuedeman@gmail.com>
2026-04-14 13:31:40 -0500

    fixes

--- src/components/home/NewItems.jsx
+++ src/components/home/NewItems.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
@@ -101,15 +102,49 @@ const NewItems = () => {
                     ))
                   : (newItems.length ? newItems : skeletonItems).map((item, index) => (
                       <div key={index} className="px-2">
-                        <NFTCard
-                          item={item}
-                          authorImage={item.authorImage || AuthorImage}
-                          authorName={item.authorName || "Unknown Author"}
-                          authorId={item.authorId}
-                          nftImage={item.nftImage || nftImage}
-                          showCountdown={!!item.expiryDate}
-                          expiryDate={item.expiryDate}
-                        />
+                        {/* Mobile view - use nft_coll structure */}
+                        <div className="d-lg-none">
+                          <div className="nft_coll">
+                            <div className="nft_wrap">
+                              <Link to={`/item-details/${item.id || item.nftId}`}>
+                                <img
+                                  src={item.nftImage || nftImage}
+                                  className="lazy img-fluid"
+                                  alt={item.title || item.name || "NFT Item"}
+                                />
+                              </Link>
+                            </div>
+                            <div className="nft_coll_pp">
+                              <Link to={`/author/${item.authorId}`}>
+                                <img
+                                  className="lazy pp-coll"
+                                  src={item.authorImage || AuthorImage}
+                                  alt={item.authorName || "Author"}
+                                />
+                              </Link>
+                              <i className="fa fa-check"></i>
+                            </div>
+                            <div className="nft_coll_info">
+                              <Link to="/explore">
+                                <h4>{item.title || item.name || "Unknown Item"}</h4>
+                              </Link>
+                              <span>{item.price ? `${item.price} ETH` : "New Item"}</span>
+                            </div>
+                          </div>
+                        </div>
+                        
+                        {/* Desktop view - use NFTCard */}
+                        <div className="d-none d-lg-block">
+                          <NFTCard
+                            item={item}
+                            authorImage={item.authorImage || AuthorImage}
+                            authorName={item.authorName || "Unknown Author"}
+                            authorId={item.authorId}
+                            nftImage={item.nftImage || nftImage}
+                            showCountdown={!!item.expiryDate}
+                            expiryDate={item.expiryDate}
+                          />
+                        </div>
                       </div>
                     ))}
               </Slider>
