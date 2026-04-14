6bb38a1c8871390213cc3a6de971d29dda2d9bfe
Cole Stuedeman <cstuedeman@gmail.com>
2026-04-13 20:43:07 -0500

    fixes

--- src/components/UI/ShareButtons.jsx
+++ src/components/UI/ShareButtons.jsx
@@ -4,15 +4,27 @@ const ShareButtons = () => {
   return (
     <div className="nft__item_share">
       <h4>Share</h4>
-      <a href="" target="_blank" rel="noreferrer">
+      <button 
+        onClick={() => window.open(`https://www.facebook.com/sharer/sharer.php?u=${encodeURIComponent(window.location.href)}`, '_blank')}
+        className="share-button"
+        aria-label="Share on Facebook"
+      >
         <i className="fa fa-facebook fa-lg"></i>
-      </a>
-      <a href="" target="_blank" rel="noreferrer">
+      </button>
+      <button 
+        onClick={() => window.open(`https://twitter.com/intent/tweet?url=${encodeURIComponent(window.location.href)}`, '_blank')}
+        className="share-button"
+        aria-label="Share on Twitter"
+      >
         <i className="fa fa-twitter fa-lg"></i>
-      </a>
-      <a href="">
+      </button>
+      <button 
+        onClick={() => window.location.href = `mailto:?subject=Check this out&body=${encodeURIComponent(window.location.href)}`}
+        className="share-button"
+        aria-label="Share via Email"
+      >
         <i className="fa fa-envelope fa-lg"></i>
-      </a>
+      </button>
     </div>
   );
 };
